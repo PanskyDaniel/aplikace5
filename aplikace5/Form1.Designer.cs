ce850f7c4de0a83293ae31ab8044ce4c3ac13866
Daniel Panský <daniel.pansky@centrum.cz>
2023-05-19 13:26:41 +0200

    Přidejte soubory projektu.

---
+++ aplikace5/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Aplikace5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.NazevSouboru = new System.Windows.Forms.TextBox();
+            this.Seznam = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Kontrola = new System.Windows.Forms.Button();
+            this.Log = new System.Windows.Forms.TextBox();
+            this.IntervalTextBox = new System.Windows.Forms.TextBox();
+            this.NastavInterval = new System.Windows.Forms.Button();
+            this.Cesta = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nazev = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Velikost = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PosledniZmena = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PocetRadku = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MaxVelikost = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MaxPocetRadku = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.Seznam)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button1.Location = new System.Drawing.Point(429, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Vybrat soubor(y)";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // NazevSouboru
+            // 
+            this.NazevSouboru.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.NazevSouboru.Location = new System.Drawing.Point(23, 26);
+            this.NazevSouboru.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.NazevSouboru.Name = "NazevSouboru";
+            this.NazevSouboru.ReadOnly = true;
+            this.NazevSouboru.Size = new System.Drawing.Size(1085, 20);
+            this.NazevSouboru.TabIndex = 2;
+            // 
+            // Seznam
+            // 
+            this.Seznam.AllowUserToAddRows = false;
+            this.Seznam.AllowUserToDeleteRows = false;
+            this.Seznam.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.Seznam.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Seznam.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Cesta,
+            this.Nazev,
+            this.Velikost,
+            this.PosledniZmena,
+            this.PocetRadku,
+            this.MaxVelikost,
+            this.MaxPocetRadku});
+            this.Seznam.Location = new System.Drawing.Point(23, 98);
+            this.Seznam.Name = "Seznam";
+            this.Seznam.RowHeadersVisible = false;
+            this.Seznam.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.Seznam.Size = new System.Drawing.Size(1085, 272);
+            this.Seznam.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button2.Location = new System.Drawing.Point(574, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 24);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Vybrat adreář(e)";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Kontrola
+            // 
+            this.Kontrola.Location = new System.Drawing.Point(319, 392);
+            this.Kontrola.Name = "Kontrola";
+            this.Kontrola.Size = new System.Drawing.Size(138, 22);
+            this.Kontrola.TabIndex = 5;
+            this.Kontrola.Text = "Kontrola";
+            this.Kontrola.UseVisualStyleBackColor = true;
+            this.Kontrola.Click += new System.EventHandler(this.Kontrola_Click);
+            // 
+            // Log
+            // 
+            this.Log.Location = new System.Drawing.Point(23, 438);
+            this.Log.Multiline = true;
+            this.Log.Name = "Log";
+            this.Log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Log.Size = new System.Drawing.Size(1085, 124);
+            this.Log.TabIndex = 6;
+            this.Log.TextChanged += new System.EventHandler(this.Log_TextChanged);
+            // 
+            // IntervalTextBox
+            // 
+            this.IntervalTextBox.Location = new System.Drawing.Point(570, 392);
+            this.IntervalTextBox.Name = "IntervalTextBox";
+            this.IntervalTextBox.Size = new System.Drawing.Size(74, 20);
+            this.IntervalTextBox.TabIndex = 7;
+            // 
+            // NastavInterval
+            // 
+            this.NastavInterval.Location = new System.Drawing.Point(665, 391);
+            this.NastavInterval.Name = "NastavInterval";
+            this.NastavInterval.Size = new System.Drawing.Size(111, 20);
+            this.NastavInterval.TabIndex = 8;
+            this.NastavInterval.Text = "Nastav interval";
+            this.NastavInterval.UseVisualStyleBackColor = true;
+            this.NastavInterval.Click += new System.EventHandler(this.NastavInterval_Click);
+            // 
+            // Cesta
+            // 
+            this.Cesta.HeaderText = "Cesta";
+            this.Cesta.Name = "Cesta";
+            this.Cesta.ReadOnly = true;
+            this.Cesta.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Cesta.Width = 300;
+            // 
+            // Nazev
+            // 
+            this.Nazev.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Nazev.HeaderText = "Název";
+            this.Nazev.Name = "Nazev";
+            this.Nazev.ReadOnly = true;
+            this.Nazev.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Velikost
+            // 
+            this.Velikost.HeaderText = "Velikost [MB]";
+            this.Velikost.Name = "Velikost";
+            this.Velikost.ReadOnly = true;
+            this.Velikost.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // PosledniZmena
+            // 
+            this.PosledniZmena.HeaderText = "Poslední změna";
+            this.PosledniZmena.Name = "PosledniZmena";
+            this.PosledniZmena.ReadOnly = true;
+            this.PosledniZmena.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.PosledniZmena.Width = 120;
+            // 
+            // PocetRadku
+            // 
+            this.PocetRadku.HeaderText = "Počet řádků";
+            this.PocetRadku.Name = "PocetRadku";
+            this.PocetRadku.ReadOnly = true;
+            this.PocetRadku.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // MaxVelikost
+            // 
+            this.MaxVelikost.HeaderText = "Maximální velikost";
+            this.MaxVelikost.Name = "MaxVelikost";
+            this.MaxVelikost.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // MaxPocetRadku
+            // 
+            this.MaxPocetRadku.HeaderText = "Maximální počet řádků";
+            this.MaxPocetRadku.Name = "MaxPocetRadku";
+            this.MaxPocetRadku.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1143, 574);
+            this.Controls.Add(this.NastavInterval);
+            this.Controls.Add(this.IntervalTextBox);
+            this.Controls.Add(this.Log);
+            this.Controls.Add(this.Kontrola);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Seznam);
+            this.Controls.Add(this.NazevSouboru);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Seznam)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox NazevSouboru;
+        private System.Windows.Forms.DataGridView Seznam;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Kontrola;
+        private System.Windows.Forms.TextBox Log;
+        private System.Windows.Forms.TextBox IntervalTextBox;
+        private System.Windows.Forms.Button NastavInterval;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cesta;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nazev;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Velikost;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PosledniZmena;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PocetRadku;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaxVelikost;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaxPocetRadku;
+    }
+}
+
---
+++ aplikace5/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aplikace5
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+        private List<string> selectedFilePaths;
+        private DateTime posledniDatumKontroly;
+        private const int defaultInterval = 60;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            posledniDatumKontroly = DateTime.MinValue;
+
+            timer = new Timer();
+            timer.Tick += Timer_Tick;
+
+            IntervalTextBox.Text = defaultInterval.ToString();
+            NastavInterval_Click(null, null);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Kontrola_Click(null, null);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Soubory logu |*.log;*.txt";
+                openFileDialog.RestoreDirectory = true;
+                openFileDialog.Multiselect = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedFilePaths = openFileDialog.FileNames.ToList();
+                    string[] fileNames = openFileDialog.SafeFileNames;
+                    NazevSouboru.Text = string.Join(", ", fileNames);
+
+                    NactiSoubory(selectedFilePaths);
+                }
+                else
+                {
+                    selectedFilePaths = new List<string>();
+                    NazevSouboru.Text = string.Empty;
+                }
+            }
+        }
+
+        private void NactiSoubory(List<string> soubory)
+        {
+            foreach (string soubor in soubory)
+            {
+                string cesta = Path.GetDirectoryName(soubor);
+                string nazev = Path.GetFileName(soubor);
+
+                long filelength = new FileInfo(soubor).Length;
+                double fileLengthMB = (double)filelength / (1024 * 1024);
+                string velikost = fileLengthMB.ToString("0.###");
+
+                DateTime filemodify = File.GetLastWriteTime(soubor);
+                string posledniZmena = filemodify.ToString("dd.MM.yyyy HH:mm:ss");
+
+                int rowCount = GetLineCount(soubor);
+                string pocetRadku = rowCount.ToString();
+
+                Seznam.Rows.Add(cesta, nazev, velikost, posledniZmena, pocetRadku);
+            }
+        }
+
+        private int GetLineCount(string filePath)
+        {
+            int count = 0;
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                while (reader.ReadLine() != null)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] filters = new[] { "*.log", "*.txt" };
+            selectedFilePaths = new List<string>();
+
+            using (FolderBrowserDialog folderDig = new FolderBrowserDialog())
+            {
+                folderDig.SelectedPath = "C:\\";
+
+                if (folderDig.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string filter in filters)
+                    {
+                        var tmp = Directory.GetFiles(folderDig.SelectedPath, filter);
+                        selectedFilePaths.AddRange(Directory.GetFiles(folderDig.SelectedPath, filter).ToList());
+                    }
+
+                    NactiSoubory(selectedFilePaths);
+                }
+            }
+        }
+
+        private void Kontrola_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in Seznam.Rows)
+            {
+                int pocetRadku = 0;
+                int maxRadku = 0;
+                double fileLengthMB = 0;
+                double maxFileSizeMB = 0;
+
+                string cesta = (string)row.Cells["Cesta"].Value;
+                string nazev = (string)row.Cells["Nazev"].Value;
+                string strPocetRadku = (string)row.Cells["PocetRadku"].Value;
+                string strMaxPocetRadku = (string)row.Cells["MaxPocetRadku"].Value;
+                string strVelikost = (string)row.Cells["Velikost"].Value;
+                string strMaxVelikost = (string)row.Cells["MaxVelikost"].Value;
+
+                DateTime filemodify = File.GetLastWriteTime(Path.Combine(cesta, nazev));
+                if (filemodify > posledniDatumKontroly) //soubor byl od poslední kontroly změněn
+                {
+
+                    if (double.TryParse(strVelikost, out fileLengthMB) && double.TryParse(strMaxVelikost, out maxFileSizeMB))
+                    {
+                        if (fileLengthMB > maxFileSizeMB)
+                        {
+                            Log.Text += string.Format("Soubor {0} má větší velikost...", nazev) + Environment.NewLine;
+                        }
+                    }
+
+                    if (int.TryParse(strPocetRadku, out pocetRadku) && int.TryParse(strMaxPocetRadku, out maxRadku))
+                    {
+                        if (pocetRadku > maxRadku)
+                        {
+                            Log.Text += string.Format("Soubor {0} má více řádků...", nazev) + Environment.NewLine;
+                        }
+                    }
+
+                }
+
+                row.Cells["PocetRadku"].Value = GetLineCount(Path.Combine(cesta, nazev)).ToString();
+
+                long filelength = new FileInfo(Path.Combine(cesta, nazev)).Length;
+                fileLengthMB = (double)filelength / (1024 * 1024);
+                row.Cells["Velikost"].Value = fileLengthMB.ToString("0.###");
+
+                row.Cells["PosledniZmena"].Value = filemodify.ToString("dd.MM.yyyy HH:mm:ss");
+            }
+
+            posledniDatumKontroly = DateTime.Now;
+        }
+
+        private void NastavInterval_Click(object sender, EventArgs e)
+        {
+            int interval = 0;
+            if (int.TryParse(IntervalTextBox.Text, out interval))
+            {
+                timer.Stop();
+                timer.Interval = interval * 1000;
+                timer.Start();
+
+            }
+        }
+
+        private void Log_TextChanged(object sender, EventArgs e)
+        {
+            Log.SelectionStart = Log.TextLength;
+            Log.ScrollToCaret();
+        }
+    }
+}
+
